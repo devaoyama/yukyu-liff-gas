6bcf8b47de2ca6b0ca563f80779d132dd750b6f2
devkeita <keita.mywork@gmail.com>
2020-12-26 16:54:12 +0900

    add dotenv

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const GasPlugin = require('gas-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
     entry: {
@@ -24,6 +25,9 @@ module.exports = {
         ]
     },
     plugins: [
-        new GasPlugin()
+        new GasPlugin(),
+        new Dotenv({
+            path: path.join(__dirname, '.env')
+        })
     ]
 }
