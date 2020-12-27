1c74e94de590ee1cdcf7c9ee8a5e5501c980e14c
devkeita <keita.mywork@gmail.com>
2020-12-27 23:04:20 +0900

    feat create sheet if not exist

--- src/spreadsheet/index.ts
+++ src/spreadsheet/index.ts
@@ -1,12 +1,18 @@
-const spreadsheet = (lineID: string) => {
+const spreadsheet = (lineID: string, name: string) => {
     // スプレッドシートを取得
     const spreadsheetID = PropertiesService.getScriptProperties().getProperty('spreadsheet_id');
     const spreadsheet = SpreadsheetApp.openById(spreadsheetID);
-    const sheet = spreadsheet.getSheetByName(lineID);
+    let sheet = spreadsheet.getSheetByName(lineID);
 
     // LINEのUSER IDをもとにシートを取得
     if (!sheet) {
         // シートが存在しなかったら作成
+        sheet = spreadsheet.insertSheet(lineID);
+        sheet.getRange('A1:A2').setValues([['名前', name]]);
+        sheet.getRange('A3:C3').setValues([['日時', 'タイプ', '理由']]);
+        sheet.getRange('A4:A').setNumberFormat('YYYY-MM-DD');
+        const rule = SpreadsheetApp.newDataValidation().requireValueInList(['1日', '半日']).build();
+        sheet.getRange('B4:B').setDataValidation(rule);
     }
 
     // シートに情報を保存
