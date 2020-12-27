9076dee1037a532ae287e24df3f86e8007cab6ba
devkeita <keita.mywork@gmail.com>
2020-12-27 22:55:55 +0900

    feat get sheet

--- src/spreadsheet/index.ts
+++ src/spreadsheet/index.ts
@@ -1,9 +1,14 @@
-const spreadsheet = () => {
+const spreadsheet = (lineID: string) => {
     // スプレッドシートを取得
+    const spreadsheetID = PropertiesService.getScriptProperties().getProperty('spreadsheet_id');
+    const spreadsheet = SpreadsheetApp.openById(spreadsheetID);
+    const sheet = spreadsheet.getSheetByName(lineID);
 
     // LINEのUSER IDをもとにシートを取得
+    if (!sheet) {
         // シートが存在しなかったら作成
-    
+    }
+
     // シートに情報を保存
 }
 
