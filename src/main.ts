e10c6d0bd78079074dd262a6f9422f022f83dd73
devkeita <keita.mywork@gmail.com>
2020-12-28 20:05:36 +0900

    feat liff verify

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,19 @@
+import verify from "./line/verify";
+
 declare var global: any;
 
 global.doGet = () => {
     return HtmlService.createHtmlOutput('Hello Gas!');
+}
+
+global.doPost = (e) => {
+    const data = JSON.parse(e.postData.getDataAsString());
+    if (!data.idToken) {
+        return ContentService.createTextOutput('IDトークンが見つかりません');
+    }
+    const res = verify(data.idToken);
+    if (!res) {
+        return ContentService.createTextOutput('IDトークンを認証できません');
+    }
+    return ContentService.createTextOutput(res);
 }
