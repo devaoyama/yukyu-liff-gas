49da7d5a71d42cc2c47aeda007f3c15f2aa57e11
devkeita <keita.mywork@gmail.com>
2020-12-30 14:01:04 +0900

    fix spreadsheet range

--- src/spreadsheet/index.ts
+++ src/spreadsheet/index.ts
@@ -4,15 +4,14 @@ const spreadsheet = (lineID: string, name: string, date: string, days: "1日" | 
     const spreadsheet = SpreadsheetApp.openById(spreadsheetID);
     let sheet = spreadsheet.getSheetByName(lineID);
 
-    // LINEのUSER IDをもとにシートを取得
     if (!sheet) {
         // シートが存在しなかったら作成
         sheet = spreadsheet.insertSheet(lineID);
-        sheet.getRange('A1:B1').setValues([['名前', name]]);
-        sheet.getRange('A3:C3').setValues([['日時', 'タイプ', '理由']]);
-        sheet.getRange('A4:A').setNumberFormat('YYYY-MM-DD');
+        sheet.getRange('A1:B2').setValues([['名前', '実際の名前'], [name, '']]);
+        sheet.getRange('A4:C4').setValues([['日時', 'タイプ', '理由']]);
+        sheet.getRange('A5:A').setNumberFormat('YYYY-MM-DD');
         const rule = SpreadsheetApp.newDataValidation().requireValueInList(['1日', '半日']).build();
-        sheet.getRange('B4:B').setDataValidation(rule);
+        sheet.getRange('B5:B').setDataValidation(rule);
     }
 
     // シートに情報を保存
